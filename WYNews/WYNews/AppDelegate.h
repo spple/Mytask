be6b5025fcadfb14801a71f38262a9435ef15fe5
spple@163.com <spple@163.com>
2017-05-15 18:46:28 +0800

    项目初始化

---
+++ WYNews/WYNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WYNews
+//
+//  Created by haoge on 17/5/15.
+//  Copyright © 2017年 haoge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WYNews/WYNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WYNews
+//
+//  Created by haoge on 17/5/15.
+//  Copyright © 2017年 haoge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
